e1fe77f8abde5f424d39b5f0c5dde7d744e23fc2
MARIANACECILIAMATOS <matosm@ar.ibm.com>
2022-04-06 21:35:35 -0300

    Base para TP1

---
+++ src/TP1.hs
@@ -0,0 +1,10 @@
+module TP1 where
+
+esMes :: Int -> Bool
+esMes = undefined
+
+hayCambioDeEstacion :: Int -> Bool
+hayCambioDeEstacion = undefined
+
+estacion :: Int -> String
+estacion = undefined
