39c9442af4b0d969d4e6450ace84300133c51b41
agusdubreucq1 <adubreucq@frba.utn.edu.ar>
2022-04-10 19:25:36 -0300

    TP1.hs

--- src/TP1.hs
+++ src/TP1.hs
@@ -1,10 +1,18 @@
 module TP1 where
 
 esMes :: Int -> Bool
-esMes = undefined
+esMes x= (x>=1) && (x<=12)
 
 hayCambioDeEstacion :: Int -> Bool
-hayCambioDeEstacion = undefined
+hayCambioDeEstacion y = (mod y 3 == 0) && (esMes y)
 
 estacion :: Int -> String
-estacion = undefined
+estacion x
+  | (x>0 && x<3) = "verano"
+  | (x>3 && x<6) = "otonio"
+  | (x>6 && x<9) = "invierno"
+  | (x>9 && x<12) = "primavera"
+estacion 3 ="verano/otonio"
+estacion 6 ="otonio/invierno"
+estacion 9 ="invierno/primavera"
+estacion 12 ="primavera/verano"
