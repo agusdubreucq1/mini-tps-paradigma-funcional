d4596e3ffde0e9f73e75577c0f1a53536cf8e6ae
agusdubreucq1 <adubreucq@frba.utn.edu.ar>
2022-04-19 17:30:43 -0300

    TP 2 modificado

--- src/TP2.hs
+++ src/TP2.hs
@@ -14,9 +14,9 @@ data Celular = Celular {
 
 promoRecarga :: Int -> Celular -> Celular
 promoRecarga monto celu
-  | esMovistar celu && cod011 celu = crearCeluMovistar011 monto celu
-  | esPersonal celu = crearCeluPersonal monto celu
-  | otherwise = crearCeluNormal monto celu
+  | esMovistar celu && cod011 celu = modificarSaldo celu (aumentarTriple monto) 
+  | esPersonal celu = modificarSaldo celu (aumentarConPlus monto)
+  | otherwise = modificarSaldo celu monto
 
 esMovistar::Celular->Bool
 esMovistar celu = (proveedor celu) == "movistar"
@@ -27,20 +27,14 @@ esPersonal celu = (proveedor celu) == "personal"
 cod011::Celular->Bool
 cod011 celu = fst (linea celu) == "011"
 
-crearCeluPersonal::Int->Celular->Celular
-crearCeluPersonal monto (Celular linea saldo proveedor) = Celular linea (aumentarConPlus saldo monto) proveedor
+modificarSaldo::Celular->Int->Celular
+modificarSaldo celu monto = celu{saldo=(saldo celu)+ monto}
 
-crearCeluMovistar011::Int->Celular->Celular
-crearCeluMovistar011 monto (Celular linea saldo proveedor) = Celular linea (aumentarTriple saldo monto) proveedor
+aumentarTriple::Int->Int
+aumentarTriple monto= monto*3
 
-crearCeluNormal::Int->Celular->Celular
-crearCeluNormal monto (Celular linea saldo proveedor) = Celular linea (saldo + monto) proveedor
-
-aumentarTriple::Int->Int->Int
-aumentarTriple saldoActual monto= saldoActual + monto*3
-
-aumentarConPlus::Int->Int->Int
-aumentarConPlus saldoActual monto = saldoActual + monto + (minimo monto 100)
+aumentarConPlus::Int->Int
+aumentarConPlus monto = monto + (minimo monto 100)
 
 minimo::Int->Int->Int
 minimo a b
