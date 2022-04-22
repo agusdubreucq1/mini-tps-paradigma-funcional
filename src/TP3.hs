320dc0ffe319d1a47031b2f309749c3f1637c165
agusdubreucq1 <adubreucq@frba.utn.edu.ar>
2022-04-22 15:34:34 -0300

    primer entrega tp3

--- src/TP3.hs
+++ src/TP3.hs
@@ -2,7 +2,7 @@ module TP3 where
 import Cafe
 
 prepararFrapuccino :: Gramos -> Cafe
-prepararFrapuccino = undefined
+prepararFrapuccino = (servirEnVaso 400).(licuar 60 120).(agregarHielo 6).(prepararCafe 80).molerGranos
 
 prepararCafeDeFiltro :: Vaso -> Gramos -> Cafe
-prepararCafeDeFiltro = undefined
+prepararCafeDeFiltro vaso= (servirEnVaso vaso).(prepararCafe 1000).molerGranos
